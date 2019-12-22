4a828d9bbc073c226bde780f9bb09cc8b8fb959c
d3vkk <donaldkk22@gmail.com>
2019-12-22 15:12:59 +0300

    feat: map json data with structs

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"./jsontesting"
+)
+
+// Books struct
+type Books struct {
+	Books []Book `json:"classicbooks"`
+}
+
+// Book struct
+type Book struct {
+	ID       int    `json:"id"`
+	Isbn     int    `json:"isbn"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Synopsis string `json:"synopsis"`
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var books Books
+	var data = jsontesting.BookData()
+
+	err := json.Unmarshal([]byte(data), &books)
+	if err != nil {
+		fmt.Println("problem:", err)
+	}
+
+	fmt.Println(books.Books[0].Author)
+
+	for i := 0; i < len(books.Books); i++ {
+		fmt.Println("ID: " + strconv.Itoa(books.Books[i].ID))
+		fmt.Println("Isbn: " + strconv.Itoa(books.Books[i].Isbn))
+		fmt.Println("Title: " + books.Books[i].Title)
+		fmt.Println("Author: " + books.Books[i].Author)
+		fmt.Println("Synopsis: " + books.Books[i].Synopsis)
+	}
+
 }
